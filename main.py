1e515213594b2b0fb3b781d44260b24490a54ff6
Matthew <matthew.g.fogarty@gmail.com>
2021-04-23 09:27:24 +0100

    "Third Commit"

--- main.py
+++ main.py
@@ -31,10 +31,24 @@ Peat = GWh[GWh['Fuel Group']=='Peat']
 Renewables = GWh[GWh['Fuel Group']=='Renewables']
 
 #merging the new datasets
-Energy = Oil.merge(Elec, on = 'Year', suffixes=('_Oil','_Elec'))\
-            .merge(NatGas, on = 'Year', suffixes=('_Oil','_NatGas'))\
-                .merge(Waste, on = 'Year', suffixes=('_Oil','_Waste'))\
-                    .merge(Coal, on = 'Year', suffixes=('_Oil','_Coal'))\
-                        .merge(Peat, on = 'Year', suffixes=('_Oil','_Peat'))\
-                            .merge(Renewables, on = 'Year', suffixes=('_Oil','_Renew'))
+Energy = Oil.merge(Elec, on = 'Year', how='left', suffixes=('_Oil', '_Elec'))
+
+Energy= Energy.merge(NatGas, on='Year')
+Energy = Energy.rename(columns={'Fuel Group':'Fuel Group_NatGas', 'Primary Energy':'Primary Energy_NatGas'})
+
+Energy= Energy.merge(Waste, on='Year')
+Energy = Energy.rename(columns={'Fuel Group':'Fuel Group_Waste', 'Primary Energy':'Primary Energy_Waste'})
+
+Energy= Energy.merge(Coal, on='Year')
+Energy = Energy.rename(columns={'Fuel Group':'Fuel Group_Coal', 'Primary Energy':'Primary Energy_Coal'})
+
+Energy= Energy.merge(Peat, on='Year')
+Energy = Energy.rename(columns={'Fuel Group':'Fuel Group_Peat', 'Primary Energy':'Primary Energy_Peat'})
+
+Energy= Energy.merge(Renewables, on='Year')
+Energy = Energy.rename(columns={'Fuel Group':'Fuel Group_Renewables', 'Primary Energy':'Primary Energy_Renewables'})
 print(Energy.columns)
+
+Energy_1 = ['Year', 'Primary Energy_Oil', 'Primary Energy_Elec','Primary Energy_NatGas','Primary Energy_Waste',
+            'Primary Energy_Coal', 'Primary Energy_Peat','Primary Energy_Renewables']
+
